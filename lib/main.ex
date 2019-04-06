0e00401ce3dd74810db98430fb35d5b53bab73c9
tania <taniadaniela1803@gmail.com>
2019-04-06 18:32:55 -0400

    Starting interactive commands.

--- lib/main.ex
+++ lib/main.ex
@@ -1,5 +1,34 @@
 defmodule RubberDux.CLI do
-  def main(_args) do
-    IO.puts("What is happening with your code dude?")
+  def main(args) do
+    args |> process_args
   end
+
+  @commands %{
+    "quit" => "Quits the simulator",
+    "ask" => "receives a question, provide an answer"
+  }
+
+  def process_args(_) do
+    IO.puts("What is happening with your code dude?")
+    print_help_message()
+    receive_command()
+  end
+
+  defp print_help_message do
+    IO.puts("\nRubber Dux supports following commands:\n")
+    @commands
+    |> Enum.map(fn({command, description}) -> IO.puts("  #{command} - #{description}") end)
+  end
+
+  defp receive_command do
+    IO.gets("\n> ")
+    |> String.trim
+    |> String.downcase
+    |> execute_command
+  end
+
+   defp execute_command("quit") do
+    IO.puts "\nSee you, cuak!"
+  end
+
 end
