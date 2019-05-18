031a2d9b4b3426a484b74940a035594d1bf1d132
tania <taniadaniela1803@gmail.com>
2019-05-18 19:53:18 -0400

    Add more answers.

--- lib/rubber_dux.ex
+++ lib/rubber_dux.ex
@@ -3,18 +3,27 @@ defmodule RubberDux do
     separated = String.split(params, " ")
     cond do
       Enum.any?(["webhook", "webhooks"], fn x -> x in separated end) ->
-        {:ok, "Are the IPs being whitelisted? \nAre the creds being rotated?"}
+        {:ok, "Are the IPs being whitelisted?\n
+Are creds being rotated?\n
+Are you in the allowed IP range?"}
       Enum.any?(["test", "tests"], fn x -> x in separated end) ->
-        {:ok, "Is the test environment configuration correct?"}
+        {:ok, "Is the test environment configuration correct?\n
+Is your test or fixture data affecting your test scenarios?"}
       Enum.any?(["inheritance", "class", "object"], fn x -> x in separated end) ->
-        {:ok, "Is the father class correct? \nAre the methods accesible by the child classes?"}
+        {:ok, "Is the father class correct?\n
+Are methods accesible by the child classes?\n
+Are the inherited methods being overwritten?"}
       Enum.any?(["routes", "route", "endpoint"], fn x -> x in separated end) ->
-        {:ok, "Are the routes defined? \nAre routes accesible?"}
+        {:ok, "Are the routes defined?\n
+Are routes accesible?"}
       Enum.any?(["var", "variables", "vars", "variables"], fn x -> x in separated end) ->
-        {:ok, "Could the variables being overwritten? \nAre the variables under the rigth scope?"}
+        {:ok, "Could the variables being overwritten?\n
+Are the variables under the right scope?"}
       # wink wink the IT Crowd  
       Enum.member?(separated, "nothing") ->
-        {:ok, "Is your browser up to date? \nWhat about a DNS issue? \nHave You Tried Turning It Off And On Again?"}
+        {:ok, "Is your browser up to date?\n
+What about a DNS issue?\n
+Have You Tried Turning It Off And On Again?"}
       true ->
         {:ok, "cuak"}  
     end
